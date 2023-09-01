4ba995c6c6754c846bf3e4f0da0dcde8d8687e73
sagar <sagar@Sagar>
2023-09-01 17:17:06 +0530

    Build B03

--- Programming/src/classwork/TestCase.java
+++ Programming/src/classwork/TestCase.java
@@ -4,6 +4,7 @@ public class TestCase
 	public static void main(String[] args) 
 	{
 		String a= "Sagar";
+		Sting b="Test";
 		String s="My Name Is Sagar";
 		/*String[] arr = s.split(" ");
 		for (int i=0; i<arr.length; i++) 
